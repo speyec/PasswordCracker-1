5bda95b814df72fe55e4e13f7823af8e5b11252c
Michael <ouatum44@gmail.com>
2025-06-23 16:49:48 -0400

    simple password cracker

--- password_cracker.py
+++ password_cracker.py
@@ -1,1 +1,29 @@
 # Password cracker
+
+import time
+
+password = input("Enter a password (only lowercase letters):  ")
+start = time.time()
+chars = 'abcdefghijklmnopqrstuvwxyz'
+guess = []
+for value in range(10):
+    a = [i for i in chars]
+    # a = []
+    # for i in chars:
+    #     a.append(i)
+    for y in range(value):
+        a = [x+i for i in chars for x in a]
+        # a = []
+        # for i in chars:
+        #     for x in a:
+        #         a.append(x + i)
+       
+    guess = guess + a
+    if password in guess:
+        break
+
+end = time.time()
+clock = str(end - start)
+
+print("Your password is: " + password)
+print("Time taken: " + clock)
